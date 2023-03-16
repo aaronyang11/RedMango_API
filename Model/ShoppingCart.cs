a08cc210003dbacb61f10aa1b8e433578370388f
YANG Jin <jyan863@aucklanduni.ac.nz>
2023-03-16 13:59:17 +1300

    create ShoppingCart and CartItem Model.

---
+++ Model/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RedMango_API.Model
+{
+	public class ShoppingCart
+	{
+		public int Id { get; set; }
+		public string UserId { get; set; }
+		
+		public ICollection<CartItem> CartItems { get; set; }
+
+		[NotMapped]
+		public double CartTotal { get; set; }
+
+        [NotMapped]
+		public string StripePaymentIntentId { get; set; }
+
+		[NotMapped]
+        public string ClientSecret { get; set; }
+    }
+
+	
+}
+
